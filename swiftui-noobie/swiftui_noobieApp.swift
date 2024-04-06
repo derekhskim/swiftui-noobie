60b3f95ae7f62d9fffbb875337d746ce7680bda3
[treasure3210] <treasure3210@gmail.com>
2024-04-06 14:28:34 -0600

    Initial project commit

---
+++ swiftui-noobie/swiftui_noobieApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_noobieApp.swift
+//  swiftui-noobie
+//
+//  Created by Derek Kim on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_noobieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
